8eef068dc8535b6696e89437752f5a793e43824d
Javier Galego <jgalegop@gmail.com>
2020-04-04 13:12:27 +0200

    Added regions

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
@@ -16,11 +14,58 @@ public class MapGenerator : MonoBehaviour
     [SerializeField]
     private MapDisplay mapDisplay = null;
 
+
+    public enum DrawMode { NoiseMap, RegionColorMap };
+    [SerializeField]
+    private DrawMode _drawMode = DrawMode.NoiseMap;
+    [SerializeField]
+    private TerrainType[] _regions = null;
+
+
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(_mapWidth, _mapHeight, _noiseScale);
 
+        Color[] colormap = SetColorMap(noiseMap);
+
         // Display
-        mapDisplay.DrawNoiseMap(noiseMap); 
+        if (_drawMode == DrawMode.NoiseMap)
+        {
+            mapDisplay.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }   
+        else if (_drawMode == DrawMode.RegionColorMap)
+        {
+            mapDisplay.DrawTexture(TextureGenerator.TextureFromColormap(colormap, _mapWidth, _mapHeight));
+        }
     }
+
+    private Color[] SetColorMap(float[,] noiseMap)
+    {
+        Color[] colormap = new Color[_mapWidth * _mapHeight];
+        for (int y = 0; y < _mapHeight; y++)
+        {
+            for (int x = 0; x < _mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                foreach (TerrainType region in _regions)
+                {
+                    if (currentHeight <= region.height)
+                    {
+                        colormap[y * _mapWidth + x] = region.color;
+                        break;
+                    }
+                }
+            }
+        }
+        return colormap;
+    }
+}
+
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
 }
---
+++ Assets/Scripts/TextureGenerator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public static class TextureGenerator
+{
+    public static Texture2D TextureFromColormap(Color[] colormap, int width, int height)
+    {
+        Texture2D texture = new Texture2D(width, height);
+        texture.SetPixels(colormap);
+        texture.filterMode = FilterMode.Point;
+        texture.wrapMode = TextureWrapMode.Clamp;
+        texture.Apply();
+        return texture;
+    }
+
+    public static Texture2D TextureFromHeightMap(float[,] heightMap)
+    {
+        int width = heightMap.GetLength(0);
+        int height = heightMap.GetLength(1);
+
+        Color[] colormap = new Color[width * height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                colormap[y * width + x] = Color.Lerp(Color.blue, Color.red, heightMap[x, y]);
+            }
+        }
+
+        return TextureFromColormap(colormap, width, height);
+    }
+}
